4f0c961a702ab4b3b8cbc3e0fc5dbd71822fbe1d
joepicard2121 <josephpicard21@gmail.com>
2026-07-26 20:10:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+    
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
