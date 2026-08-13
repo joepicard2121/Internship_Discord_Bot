cbaceadb071de98c5bb16a875528094ecb409e4c
joepicard2121 <josephpicard21@gmail.com>
2026-08-13 15:09:30 -0400

    Changes
    Changed from using websited like indeed and linkedin to using a website called adzuna to look for it internship postings. will eventually try to do indeed and linkedin. Also added some test commands and got my api keys

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import aiohttp
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+adzuna_app_id = os.getenv('ADZUNA_APP_ID')
+adzuna_app_key = os.getenv('ADZUNA_APP_KEY')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
@@ -18,8 +21,19 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f"We are ready to go in, {bot.user.name}")
 
+#Welcoming new members
 @bot.event
-async def on_memebr_join(member):
+async def on_member_join(member):
     await member.send(f"Welcome to the server {member.name}")
+    
+#Test command
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+    
+#Job search command
+@bot.command()
+async def jobs(ctx):
+    await ctx.send("🔎 Searching for IT internships...")
     
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
